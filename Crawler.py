0cdb872fa69d7da74c0be064ca399e68bb634c39
AdrielFreud <gamerbra39@gmail.com>
2018-10-05 09:45:32 -0300

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -36,6 +36,9 @@ Powered by Adriel Freud\n"""
 
 parse = argparse.ArgumentParser(description="For Get Informations of WebSite")
 parse.add_argument("-u", "--url", help="Url for get Informations! ")
+parse.add_argument("-p", "--proxy", help="Set a proxy! ")
+parse.add_argument("-c", "--cookie", help="Set a Cookie ")
+parse.add_argument("-d", "--data", help="Set a forms ")
 args = parse.parse_args()
 
 header = {'user-agent': 'Mozilla/5.0 (X11; Linux i686; rv:43.0) Gecko/10100101 Firefox/43.0 Iceweasel/43.0.4'}
@@ -44,20 +47,19 @@ def printar_detalhes(url):
 	IP = socket.gethostbyname(url.strip('https://'))
 	req = requests.get('http://ip-api.com/json/'+IP, headers=header)
 	Geo = json.loads(req.text)
-	print('')
-	print('IP: %s'%Geo['query']+'\n')
-	print('Country: %s'%Geo['country']+'\n')
-	print('Country code: %s'%Geo['countryCode']+'\n')
-	print('Region: %s'%Geo['regionName']+'\n')
-	print('Region code: %s'%Geo['region']+'\n')
-	print('City: %s'%Geo['city']+'\n')
-	print('Zip Code: %s'%Geo['zip']+'\n')
-	print('Latitude: %s'%Geo['lat']+'\n')
-	print('Longitude: %s'%Geo['lon']+'\n')
-	print('Timezone: %s'%Geo['timezone']+'\n')
-	print('ISP: %s'%Geo['isp']+'\n')
-	print('Organization: %s'%Geo['org']+'\n')
-	print('AS number/name: %s'%Geo['as']+'\n')
+	print('\nIP: %s\n'%Geo['query'])
+	print('Country: %s\n'%Geo['country'])
+	print('Country code: %s\n'%Geo['countryCode'])
+	print('Region: %s\n'%Geo['regionName'])
+	print('Region code: %s\n'%Geo['region'])
+	print('City: %s\n'%Geo['city'])
+	print('Zip Code: %s\n'%Geo['zip'])
+	print('Latitude: %s\n'%Geo['lat'])
+	print('Longitude: %s\n'%Geo['lon'])
+	print('Timezone: %s\n'%Geo['timezone'])
+	print('ISP: %s\n'%Geo['isp'])
+	print('Organization: %s\n'%Geo['org'])
+	print('AS number/name: %s\n'%Geo['as'])
 
 def email_extrator(url):
 	print("\n\033[1;36m<==================== Emails! ====================>")
@@ -84,65 +86,62 @@ def whois(url):
 		bs = BeautifulSoup(html, 'lxml')
 		div = bs.find_all('pre', {'class':'df-raw'})
 		for divs in div:
-			print('\033[1;36m<==================== info ==================>\n\n%s'%divs.get_text())
+			print('\033[1;36m<==================== info ==================>\n\n%s'%divs.get_text().encode('utf-8'))
 
 def capture(url):
 	req = requests.get(url, headers=header)
 	code = req.status_code
-	html = req.text
 	if code == 200:
+		html = req.text
 		print("\n[*]Request Succefully!\n")
 		bt = BeautifulSoup(html, "lxml")
-
-		a_ref = bt.find_all('a')
-		meta = bt.find_all('link')
-		try:
-			href = bt.link['href']
-			img = bt.img['src']
-			if href != None:
-				print("\033[1;36m<==================== Links ====================>\n\n")
-				if 'http' in href:
-					print("\033[31m"+t+"[==>] Links Externos: %s"%href)
-
-				print("\033[31m"+t+"[==>] Links Externos: http://%s"%href)
-				if img != None:
-					print("\033[31m"+t+"[==>] Links Locais: http://%s"%img)
-		except:
-			pass
-
-		if a_ref != None:
-			for link in a_ref:
-				try:
-					if 'http' in link['href']:
-						print("\033[31m"+t+"[==>] Link: %s"%link['href'])
-				except:
-					pass
-
-		if meta != None:
-			print("\033[1;36m<================== Information ==================>\n\n")
-			for link in meta:
-				try:
-					print("\033[31m"+t+"[==>] Information: %s"%link['href'])
-					print("")
-				except:
-					pass
+		urls = re.findall('(?<=href=["\'])https?://.+?(?=["\'])', html)
+		print("\033[1;36m<==================== Links ====================>\n\n")
+		for u in urls:
+			print("\033[31m"+t+"[==>] Links: %s"%u)
 	else:
 		print("\n\033[31m[!]Request Failed, Exiting Program...\n ")
-		sleep(5)
-		sys.exit()
+		sleep(3)
+		exit(1)
+
+def grabbining(url, proxy, cookie, form):
+
+	exec("forms = %s"%form)
+	esc = raw_input("GET/POST | [p][g] | \\[G]: ")
+	if esc.lower() == "p":
+		req = requests.post(url, headers=header, proxies={'http': proxy,'https': proxy}, params=forms, cookies={'Cookie':cookie})
+	else:
+		req = requests.get(url, headers=header, proxies={'http': proxy,'https': proxy}, params=forms, cookies={'Cookie':cookie})
+
+	code = req.status_code
+	if code == 200:
+		html = req.text
+		print("\n[*]Request Succefully!\n")
+		print("\033[1;36m<==================== Information ====================>\n\n\n")
+		print(html)
+	else:
+		print("\n\033[31m[!]Request Failed, Exiting Program...\n ")
+		sleep(3)
+		exit(1)
 
 if args.url:
 	print(menu)
-	req = requests.get(args.url, headers=header)
-	c = urllib2.urlopen(args.url)
-	print(c.info())
-	capture(args.url)
-	try:
-		printar_detalhes(args.url)
-	except:
-		print("Impossivel Resolver Servidor DNS!\n")
-	whois(args.url)
-	email_extrator(args.url)
+	if len(sys.argv) > 3:
+		url = sys.argv[2]
+		proxy = sys.argv[4]
+		cookie = sys.argv[6]
+		parametros = sys.argv[8]
+		grabbining(url, proxy, cookie, parametros)
+	else:
+		print(urllib2.urlopen(args.url).info())
+		capture(args.url)
+
+		try:
+			printar_detalhes(args.url)
+		except:
+			pass
+		whois(args.url)
+		email_extrator(args.url)
 else:
 	print(menu)
 	parse.print_help()
